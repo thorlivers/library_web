ed39c4cabcdc2a6f650baf50505396a52923ab80
Thanapat Waewsri <43126122+thorlivers@users.noreply.github.com>
2019-03-20 14:53:53 +0700

    Update library.sql

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 12, 2019 at 08:18 AM
+-- Generation Time: Mar 20, 2019 at 08:53 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -33,29 +33,13 @@ CREATE TABLE `book` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `booking`
---
-
-CREATE TABLE `booking` (
-  `booking_id` smallint(6) NOT NULL,
-  `book_id` smallint(6) NOT NULL,
-  `bo_id` smallint(6) NOT NULL,
-  `st_id` tinyint(3) NOT NULL,
-  `booking_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `period_date` datetime NOT NULL,
-  `status` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `book_amount`
 --
 
 CREATE TABLE `book_amount` (
   `book_a_id` smallint(6) NOT NULL,
   `book_a_code` varchar(15) NOT NULL,
-  `book_id` smallint(6) NOT NULL
+  `book_isbn` varchar(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -78,7 +62,7 @@ CREATE TABLE `book_category` (
 CREATE TABLE `borrower` (
   `bo_id` smallint(6) NOT NULL,
   `code` varchar(11) NOT NULL,
-  `id_card_num` bigint(13) NOT NULL,
+  `id_card_num` varchar(13) NOT NULL,
   `prefix_id` tinyint(2) NOT NULL,
   `fname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
@@ -97,7 +81,7 @@ CREATE TABLE `borrower` (
 
 CREATE TABLE `borrowing` (
   `borrow_id` smallint(6) NOT NULL,
-  `book_id` smallint(6) NOT NULL,
+  `book_a_code` varchar(15) NOT NULL,
   `bo_id` smallint(6) NOT NULL,
   `st_id` tinyint(3) NOT NULL,
   `borrow_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -160,20 +144,12 @@ ALTER TABLE `book`
   ADD KEY `book_isbn` (`book_isbn`);
 
 --
--- Indexes for table `booking`
---
-ALTER TABLE `booking`
-  ADD PRIMARY KEY (`booking_id`),
-  ADD KEY `book_id` (`book_id`),
-  ADD KEY `bo_id` (`bo_id`),
-  ADD KEY `st_id` (`st_id`);
-
---
 -- Indexes for table `book_amount`
 --
 ALTER TABLE `book_amount`
   ADD PRIMARY KEY (`book_a_id`),
-  ADD KEY `book_id` (`book_id`);
+  ADD KEY `book_isbn` (`book_isbn`),
+  ADD KEY `book_a_code` (`book_a_code`);
 
 --
 -- Indexes for table `book_category`
@@ -194,9 +170,9 @@ ALTER TABLE `borrower`
 --
 ALTER TABLE `borrowing`
   ADD PRIMARY KEY (`borrow_id`),
-  ADD KEY `book_id` (`book_id`),
   ADD KEY `bo_id` (`bo_id`),
-  ADD KEY `st_id` (`st_id`);
+  ADD KEY `st_id` (`st_id`),
+  ADD KEY `book_a_code` (`book_a_code`);
 
 --
 -- Indexes for table `departments`
@@ -227,12 +203,6 @@ ALTER TABLE `staff`
 --
 ALTER TABLE `book`
   MODIFY `book_id` smallint(6) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `booking`
---
-ALTER TABLE `booking`
-  MODIFY `booking_id` smallint(6) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `book_amount`
@@ -281,18 +251,10 @@ ALTER TABLE `book`
   ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`book_category_id`) REFERENCES `book_category` (`book_category_id`);
 
 --
--- Constraints for table `booking`
---
-ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`bo_id`) REFERENCES `borrower` (`bo_id`),
-  ADD CONSTRAINT `booking_ibfk_3` FOREIGN KEY (`st_id`) REFERENCES `staff` (`st_id`),
-  ADD CONSTRAINT `booking_ibfk_4` FOREIGN KEY (`booking_id`) REFERENCES `book` (`book_id`);
-
---
 -- Constraints for table `book_amount`
 --
 ALTER TABLE `book_amount`
-  ADD CONSTRAINT `book_amount_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`);
+  ADD CONSTRAINT `book_amount_ibfk_1` FOREIGN KEY (`book_isbn`) REFERENCES `book` (`book_isbn`);
 
 --
 -- Constraints for table `borrower`
@@ -307,7 +269,7 @@ ALTER TABLE `borrower`
 ALTER TABLE `borrowing`
   ADD CONSTRAINT `borrowing_ibfk_2` FOREIGN KEY (`bo_id`) REFERENCES `borrower` (`bo_id`),
   ADD CONSTRAINT `borrowing_ibfk_3` FOREIGN KEY (`st_id`) REFERENCES `staff` (`st_id`),
-  ADD CONSTRAINT `borrowing_ibfk_4` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`);
+  ADD CONSTRAINT `borrowing_ibfk_4` FOREIGN KEY (`book_a_code`) REFERENCES `book_amount` (`book_a_code`);
 
 --
 -- Constraints for table `staff`
